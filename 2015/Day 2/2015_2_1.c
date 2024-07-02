9d9d06688b8a0e05769db8015eeb7c3d4db81935
Mark Triassi <markjtriassi@gmail.com>
2024-07-02 09:34:43 -0700

    use sscanf instead of strtok

--- 2015/Day 2/2015_2_1.c
+++ 2015/Day 2/2015_2_1.c
@@ -1,41 +1,39 @@
 #include <stdio.h>
 #include <string.h>
 
-
-
-
 int main(){
     FILE *input;
     char line[25];
-    char *array[3];
-    int i = 0;
-    int sum = 0;
-
+    char array[3];
+    long int sum = 0;
+  
 
     input = fopen("AOC_2015_2_1.txt", "r");
 
-    do{
-        // go line by line 
-        fgets(line, 20, input);
+    // go line by line 
+    while (fgets(line, 20, input) != NULL) {
+
+        //split the line and extract l, w, h
+        int length;
+        int width;
+        int height;
+        sscanf(line, "%dx%dx%d", &length, &width, &height);
+
+        //find surface area of box
+        int surface_area = (2*length*width) + (2*width*height) + (2*height*length);
         
-        // split each line by delimiter "x"
-        char *token = strtok(line, "x");
-        while (token != NULL){
-            array[i++] = token;
-            token = strtok(NULL, "x");
-        }
-        // 2*l*w + 2*w*h + 2*h*l
-        sum += (int)(array[0]) * (int)array[1] * 2;
-        sum += (int)(array[1]) * (int)array[2] * 2;
-        sum += (int)(array[0]) * (int)array[2] * 2;
-        
-        // + the area of the smallest side
-        int min = (int)(array[0]);
-        for (int start = 1; start < 3; start++){
-            if ((int)array[start] < min)
-                min = (int)array[start];
-        }
-        printf("%d", sum);
-        printf("%d", min);
-    } while(0);
+        //add area of smallest side for slack
+        int temp = (length < width) ? length : width;
+        int min = (temp < height) ? temp : height;
+        // sum += min;
+
+        sum += min + surface_area;
+        // printf("%d \n", length);
+        // printf("%d \n", width);
+        // printf("%d \n", height);
+        // printf("%d \n", min);
+        // printf("%d \n", surface_area);
+        // printf("%d \n", sum);
+    }
+    printf("%d \n", sum);
 }
